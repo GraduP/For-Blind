190bcddeeca9af6fb0335800abb21f76fa47a0f6
kh <rlaruagh030@gmail.com>
2022-05-23 07:14:07 +0900

    merged some files

--- app/src/main/java/com/example/graduate_project_tmap/BusForm.java
+++ app/src/main/java/com/example/graduate_project_tmap/BusForm.java
@@ -16,7 +16,7 @@ public class BusForm extends AppCompatActivity {
         busFormBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                Intent intent = new Intent(getApplicationContext(), SignInActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/graduate_project_tmap/Login2.java
+++ app/src/main/java/com/example/graduate_project_tmap/Login2.java
@@ -8,11 +8,26 @@ import android.widget.Button;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Login2 extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login2);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser() != null){
+            Intent intent = new Intent(Login2.this, MainActivity.class);
+            startActivity(intent);
+        }
+
+
         Button busLogin = (Button) findViewById(R.id.buslogin);
         Button blindLogin = (Button) findViewById(R.id.blindlogin);
 
@@ -32,5 +47,6 @@ public class Login2 extends AppCompatActivity {
             }
         });
     }
+
 
 }

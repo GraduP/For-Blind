435aaa0db3b9be4396cce1e989343b11e1aac8b4
kh <rlaruagh030@gmail.com>
2022-05-23 08:43:47 +0900

    firestore added user info

--- app/src/main/java/com/example/graduate_project_tmap/BlindForm.java
+++ app/src/main/java/com/example/graduate_project_tmap/BlindForm.java
@@ -1,23 +1,69 @@
 package com.example.graduate_project_tmap;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.graduate_project_tmap.databinding.BlindformBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Map;
+
 public class BlindForm extends AppCompatActivity {
+
+    Map<String, Object> user;
+    BlindformBinding binding;
+    FirebaseFirestore db;
+
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.blindform);
+        binding = BlindformBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        db = FirebaseFirestore.getInstance();
+
         Button blindFormBtn = (Button) findViewById(R.id.blindFormBtn);
         blindFormBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Intent intent = new Intent(getApplicationContext(), SignInActivity.class);
-                startActivity(intent);
+                if (!binding.blindName.getText().toString().isEmpty() && !binding.blindAge.getText().toString().isEmpty() && !binding.blindSex.getText().toString().isEmpty()) {
+                    user.put("username", binding.blindName.getText().toString());
+                    user.put("age", binding.blindAge.getText().toString());
+                    user.put("gender", binding.blindSex.getText().toString());
+
+                    db.collection("users")
+                            .add(user)
+                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                                    Log.d(TAG, "onSuccess: " + documentReference.getId());
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.w(TAG, "onFailure: ", e);
+                                }
+                            });
+
+                    Intent intent = new Intent(getApplicationContext(), SignInActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(BlindForm.this, "정보를 모두 입력해주세요.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
--- app/src/main/java/com/example/graduate_project_tmap/BusForm.java
+++ app/src/main/java/com/example/graduate_project_tmap/BusForm.java
@@ -1,23 +1,76 @@
 package com.example.graduate_project_tmap;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.graduate_project_tmap.databinding.ActivitySignInBinding;
+import com.example.graduate_project_tmap.databinding.BusformBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class BusForm extends AppCompatActivity {
+
+    Map<String, Object> driver;
+    BusformBinding binding;
+    FirebaseFirestore db;
+
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.busform);
+        binding = BusformBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        db = FirebaseFirestore.getInstance();
+
         Button busFormBtn = (Button) findViewById(R.id.busFormBtn);
+
+        driver = new HashMap<>();
+
         busFormBtn.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view){
-                Intent intent = new Intent(getApplicationContext(), SignInActivity.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                if (binding.busName.getText().toString().isEmpty() && !binding.busage.getText().toString().isEmpty()
+                        && !binding.busSex.getText().toString().isEmpty() && !binding.busNum.getText().toString().isEmpty()) {
+
+                    driver.put("username", binding.busName.getText().toString());
+                    driver.put("age", binding.busage.getText().toString());
+                    driver.put("gender", binding.busSex.getText().toString());
+                    driver.put("busNumber", binding.busNum.getText().toString());
+
+                    db.collection("drivers")
+                            .add(driver)
+                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                                    Log.d(TAG, "onSuccess: " + documentReference.getId());
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.w(TAG, "onFailure: ", e);
+                                }
+                            });
+
+                    Intent intent = new Intent(getApplicationContext(), SignInActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(BusForm.this, "정보를 모두 입력해주세요.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
